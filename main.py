93f68727794d9aa8247ce3757e3cca28393af1ed
Christopher M Bradley <declarationcb@gmail.com>
2018-01-18 13:11:30 -0500

    update#003
    Removed probability function for now and cleaned up original file

--- main.py
+++ main.py
@@ -1,63 +1,39 @@
-# Christopher M. Bradley
-# Version 1.0
+# PROMET V1.0
 from bs4 import BeautifulSoup as Soup
 from urllib.request import urlopen
 from collections import Counter
 import re
 import csv
 
-pageNumber = 1
-rawDataA = []
-rawDataB = []
-DATA = {}
+PAGENUMBER = 1
+ARAWDATA = []
+BRAWDATA = []
+OFFICIALDATA = {}
 
-while pageNumber <= 1:  # Change value based on how far back you want to go in (pages)
-    counter = 0
-    url = urlopen('https://www.usamega.com/mega-millions-history.asp?p={}'.format(pageNumber))
-    uRaw = url.read()
-    url.close()
-    uSoup = Soup(uRaw, "html.parser")
+while PAGENUMBER <= 83:  # Our way of filtering through pages
+    COUNTER = 0  # We will need this later
+    url = urlopen('https://www.usamega.com/mega-millions-history.asp?p={}'.format(PAGENUMBER))
+    RAW = url.read()  # Reads data into variable
+    url.close()  # Closes connection
+    PARSED = Soup(RAW, 'html.parser')  # (DATA, Type of Parser)
 
-    for line in uSoup.findAll('td', {'align': 'right'}):
-        if 'mega-millions-drawing.asp?d=' in str(line):
-            raw = re.findall("d=(.*?)\">", str(line))
-            for u in raw:
-                rawDataA.append(u)
+    for line in PARSED.findAll('td', {'align': 'right'}):  # Finds all the 'td' tags with align:right
+        if 'mega-millions-drawing.asp?d=' in str(line):  # Checks if tag has those char
+            pRAW = re.findall('d=(.*?)\">', str(line))  # Gathers only the dates from that text
+            for pline in pRAW:
+                ARAWDATA.append(pline)  # Stores data in list for mutation later
 
-    for line in uSoup.findAll('td', {'align': 'center'}):
-        if '<strong>' in str(line) and 'nowrap' in str(line):
-            raw = re.findall("<b>(.*?)</b>", str(line))
-            for u in raw:
-                rawDataB.append(u.replace(" · ", " "))
+    for line in PARSED.findAll('td', {'align': 'center'}):
+        if '<strong>' in str(line) and 'nowrap' in str(line):  # Needs to be setup this long way
+            pRAW = re.findall('<b>(.*?)</b>', str(line))
+            for pline in pRAW:
+                BRAWDATA.append(pline.replace(" · ", " "))
 
-    for value in rawDataA:
-        DATA[value] = rawDataB[counter]
-        counter += 1
-
-    pageNumber += 1
+    for date in ARAWDATA:
+        OFFICIALDATA[date] = BRAWDATA[COUNTER]  # For every date it will give it value of the numbers
+        COUNTER += 1
+    PAGENUMBER += 1
 
 with open('lotto.csv', 'w') as data:
     file = csv.writer(data)
-    file.writerows(DATA.items())
-
-
-def probability(list):
-    BUFFED = []
-    for i in list:
-        buffer = i.split()
-        for b in buffer:
-            BUFFED.append(b)
-    STORE = Counter(BUFFED)
-# Uncomment if you want to see DATA in terminal
-#    for key, value in STORE.items():
-#        print('NUMBER: {0} | OCCURRED: {1} times'.format(key, value))
-    with open('occurrence.csv', 'w') as data:
-        file = csv.writer(data)
-        file.writerows(STORE.items())
-
-
-probability(rawDataB)
-
-# Uncomment if you want to see DATA in terminal
-# for key, value in DATA.items():
-#   print('DATE: {0} | TICKET#: {1}'.format(key, value))
+    file.writerows(OFFICIALDATA.items())
